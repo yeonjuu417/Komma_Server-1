822df7c94d7acc429b044a00641e4ca536190e01
BaeSeokJae <bsjp9400@gmail.con>
2021-01-14 14:51:28 +0900

    [Server] <feature>: login
    typeorm을 활용한 login 기능 구현 중
    
    Resolves #25

--- src/controllers/users/login.ts
+++ src/controllers/users/login.ts
@@ -1,14 +1,36 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
+import { getManager } from "typeorm";
+import User from "../../entity/User";
+import crypto from "crypto";
 import "dotenv/config";
-import passport from "passport";
-import {signres} from "../../utils";
 
-export default async (req: Request, res: Response, next: NextFunction): Promise<void | Response> => {
-  passport.authenticate(//local strategy 호출 
-    "local",
-    (err: Error, data: object, info: object): Promise<void> => {
-      signres(req, res, next, err, data, info, true);
-      return;
-    }
-  )(req, res, next);
+export default async (req: Request, res: Response) => {
+  const email: string = req.body.email;
+  const password: string = req.body.password;
+
+  const user = await getManager()
+    .createQueryBuilder(User, "User")
+    .where({ email: email })
+    .getOne();
+
+  if (!user) {
+    res.status(400).send({ data: null, message: "not authorized" })
+  } else {
+    const { salt } = user;
+    const createHashedPassword = (plainpassword) =>
+      new Promise(async (resolve, reject) => {
+        crypto.pbkdf2(plainpassword, salt, 1000, 64, 'sha512', (err, key) => {
+          if (err) reject(res.status(400).send({ message: "hashPwd exists" }));
+          resolve(key.toString('base64'));
+        });
+      });
+    const hashPwd = await createHashedPassword(password);
+    await getManager()
+    .createQueryBuilder(User, "User")
+    .where({ password: password })
+    .getOne()
+    .then(data => {
+      // res.send({message : "Login successfully" })
+    })
+  }
 };
--- src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-import signres from "./sign/signres";
-
-export {
-    signres
-}
