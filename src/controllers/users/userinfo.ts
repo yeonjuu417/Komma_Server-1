6a11e97c5f904313ab0e2a1bdbd6192d45a4c5e8
BaeSeokJae <bsjp9400@gmail.con>
2021-01-16 23:47:34 +0900

    [Server] <refactor>: userInfo jointable
    innerjoin 사용하여 playlist, savesong 테이블과 user테이블을 join하여 userinfo 응답.
    
    Resolves #50

--- src/controllers/users/userinfo.ts
+++ src/controllers/users/userinfo.ts
@@ -1,40 +1,32 @@
 import { Request, Response } from "express";
-import Playlist from "../../entity/Playlist";
-import Savesong from "../../entity/Savesong";
-import jwt from "jsonwebtoken";
+import * as jwt from "jsonwebtoken";
 import { getManager } from "typeorm";
+import User from "../../entity/User";
 
 export default async (req: Request, res: Response) => {
-    const authorization = req.headers['authorization'];
-    if (!authorization) {
-        res.status(400).send({ "data": null, "message": "invalid access token" });
-      } else {
-        const token = authorization.split(' ')[1];// barer 빼고 토큰만 가져오기 
-        const data:any = jwt.verify(token,process.env.ACCESS_SECRET);//토큰 유효한지 확인하는 작업 verify 토큰 내용 가져오기 
-        console.log(data)
+  const authorization = req.headers['authorization'];
+  if (!authorization) {
+    res.status(400).send({ "data": null, "message": "invalid access token" });
+  } else {
+    const token = authorization.split(' ')[1];// barer 빼고 토큰만 가져오기 
+    const data: any = jwt.verify(token, process.env.ACCESS_SECRET);//토큰 유효한지 확인하는 작업 verify 토큰 내용 가져오기 
 
-        const playlist = await getManager()
-        .getRepository(Playlist)
-        .createQueryBuilder("playlist")
-        .leftJoinAndSelect("playlist.user", "playlist")
-        .getMany();
+    const UserInfo = await getManager()
+      .createQueryBuilder(User, "user")
+      .innerJoinAndSelect("user.playlists", "playlist")
+      .innerJoinAndSelect("playlist.savesongs", "savesong")
+      .where("user.id = :id", { id: data.id})
+      .getOne();
 
-        // const song = await getManager()
-        // .createQueryBuilder(Savesong, "Savesong")
-        // .where({ playList: playlist.id })
-        // .getMany();
-
-
-        res.status(200).send({
-        "userInfo": { 
-                id: data.id, 
-                email: data.email,
-                username: data.username,
-                darkmode: data.darkMode,
-                sitecolor: data.siteColor,
-               },
-       "playlists": playlist
-        })
-    }
+    res.status(200).send({
+      "userInfo": {
+        id: UserInfo.id,
+        email: UserInfo.email,
+        username: UserInfo.username,
+        darkmode: UserInfo.darkMode,
+        sitecolor: UserInfo.siteColor,
+      },
+      "playlists": UserInfo.playlists
+    })
+  }
 }
-
