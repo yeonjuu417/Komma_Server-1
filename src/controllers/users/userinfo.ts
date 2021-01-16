483bbb96b463ae0343e4fd41a091c031aa133f99
jeongyeonju <yeonjuu417@icloud.com>
2021-01-16 19:13:40 +0900

    [Server] <feature>: userinfo.ts
    유저 정보 출력 하기 구현
    
    Resolves : #38

--- src/controllers/users/userinfo.ts
+++ src/controllers/users/userinfo.ts
@@ -1,6 +1,5 @@
 import {  Request, Response } from "express";
 import Playlist from "../../entity/Playlist";
-import Usersiteoption from "../../entity/Usersiteoption";
 import Savesong from "../../entity/Savesong";
 import jwt from 'jsonwebtoken'
 import { getManager } from "typeorm";
@@ -15,48 +14,27 @@ export default async (req: Request, res: Response) => {
         const data:any = jwt.verify(token,process.env.ACCESS_SECRET);//토큰 유효한지 확인하는 작업 verify 토큰 내용 가져오기 
         console.log(data)
 
-        const siteoption = await getManager()
-        .createQueryBuilder(Usersiteoption, "Usersiteoption")
-        .where({ userid: data.id })
-        .getOne();
-
         const playlist = await getManager()
-        .createQueryBuilder(Playlist, "Playlist")
-        .where({ userid: data.id })
-        .getOne();
-
-        const song:Array<any> = await getManager()
-        .createQueryBuilder(Savesong, "Savesong")
-        .where({ playList: playlist.id })
+        .getRepository(Playlist)
+        .createQueryBuilder("playlist")
+        .leftJoinAndSelect("playlist.user", "playlist")
         .getMany();
 
+        // const song = await getManager()
+        // .createQueryBuilder(Savesong, "Savesong")
+        // .where({ playList: playlist.id })
+        // .getMany();
 
-        // 유저한명에 대해서 플레이리스트도 여러개 존재
-        // 한개의 플레이리스트에 대해서도 여러곡 존재 
-        // 나타내는법 ... 
 
         res.status(200).send({
         "userInfo": { 
                 id: data.id, 
                 email: data.email,
-                username: data.username
+                username: data.username,
+                darkmode: data.darkMode,
+                sitecolor: data.siteColor,
                },
-        "userSetting": {
-                  darkmode: siteoption.darkMode,
-                  sitecolor: siteoption.siteColor,
-                },
-       "playlists": {
-         "id": playlist.id,
-         "title" :playlist.title,
-         "list": song
-            
-                //    {
-                //      id: song.id,
-                //      title: song.title,
-                //      soundFile: song.soundFile,
-                //      customVolume: song.customVoulume,
-                //    },
-            }
+       "playlists": playlist
         })
     }
 }
